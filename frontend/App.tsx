ac928ff6d86c0227fb28c765b5281da778e48935
Deanm-pixel <dean.lustek10@gmail.com>
2025-06-23 08:46:50 +0200

    Create App.tsx

---
+++ frontend/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Dashboard from './pages/Dashboard';
+import CardPage from './pages/CardPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/card/:id" element={<CardPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
